c457c8affe27276c8563927ca310ed11cbca23b4
Shoaib Merchant <shoaib@actonate.com>
2017-01-31 19:33:40 +0530

    jscs and jshint added

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,10 +6,20 @@
  */
 
 
-function User() {
+/**
+ * User Controller (to handle user class)
+ *
+ * @public
+ *
+ * @memberof   sails-starter
+ *
+ * @author     shoaibmerchant
+ *
+ * @class      User
+ * @param      {Object}  options  The options to initiatlize the class
+ */
+function User(options) {
 	var log = Logger.child({module: 'User'});
 }
-
-
 
 module.exports = User();
