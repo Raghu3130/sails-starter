e721b7043b7a4d511e210c8cb882941ed4532c7d
Shoaib Merchant <shoaib@actonate.com>
2017-01-31 17:50:24 +0530

    added logger

---
+++ api/controllers/UserController.js
@@ -0,0 +1,15 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+
+function User() {
+	var log = Logger.child({module: 'User'});
+}
+
+
+
+module.exports = User();
